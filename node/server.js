661da04bab0990759f5af0de30633d508ac2a7b0
Vamshi@Mac <vamshi4001@gmail.com>
2016-05-03 22:29:23 -0400

    Added required headers for CORS issue.

--- node/server.js
+++ node/server.js
@@ -8,9 +8,26 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+app.use(function (req, res, next) {
 
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8000');
 
-app.get('/employees', employee.findAll);
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
+
+app.get('/employees.json', employee.findAll);
 // app.get('/employees/:id', employee.findById);
 // app.post('/employees', employee.addEmployee);
 // app.put('/employees/:id', employee.updateEmployee);
